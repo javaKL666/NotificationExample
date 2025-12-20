fbb9960afbb806d873f226d01a20fc43790c83ba
javaKL666 <xb6666661979@outlook.com>
2025-12-20 15:34:23 +0800

    Update Example.lua

--- Revenant/Example.lua
+++ Revenant/Example.lua
@@ -1,1 +1,10 @@
+local RevenantNotificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/Revenant", true))()
 
+RevenantNotificationLibrary.DefaultColor = Color3.fromRGB(255,255,0)
+
+RevenantNotificationLibrary:Notification({
+    Text = "Example Example Example",
+    Duration = 10
+})
+
+wait (1)
