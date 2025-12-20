d8ee6be15a3f0e95552d967f78791612e37732fa
javaKL666 <xb6666661979@outlook.com>
2025-12-20 15:38:29 +0800

    Create Example.lua

---
+++ Cation/Example.lua
@@ -0,0 +1,5 @@
+local NotifiCationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/IceMinisterq/Notification-Library/Main/Library.lua"))()
+
+NotifiCationLibrary:SendNotification("Success", "Example Success", 5)
+
+NotifiCationLibrary:SendNotification("Error", "Example Error", 5)
