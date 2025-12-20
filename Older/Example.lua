7dbdc204bfbe2a24e2f229af1cca6353a144cf7a
javaKL666 <xb6666661979@outlook.com>
2025-12-20 15:43:01 +0800

    Create Example.lua

---
+++ Older/Example.lua
@@ -0,0 +1,7 @@
+local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))() 
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
+
+Notification:Notify(
+        {Title = "loadstring(game:HttpGet("Title Example", Description = "Txet Example"},
+        {OutlineColor = Color3.fromRGB(0, 0, 0),Time = 9, Type = "image"},
+        {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(258, 58, 68)})
