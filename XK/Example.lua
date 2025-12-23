23604cbea4cc8b24916b66025a95ca7fe0b667bd
javaKL666 <xb6666661979@outlook.com>
2025-12-23 18:25:18 +0800

    Create Example.lua

---
+++ XK/Example.lua
@@ -0,0 +1,3 @@
+local XKNotification = loadstring(game:HttpGet(('https://raw.githubusercontent.com/DevSloPo/Notification/refs/heads/main/Source.lua')))()
+
+XKNotification:Notification("Title", "Txet", 5)
