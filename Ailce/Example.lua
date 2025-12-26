586d0e766971e9a7ecdec38d1c439d9fab4f19a4
javaKL666 <xb6666661979@outlook.com>
2025-12-26 20:26:48 +0800

    Add NotifySystem functionality to Example.lua

---
+++ Ailce/Example.lua
@@ -0,0 +1,9 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TheHunterSolo1/NotifySystem/main/Main.lua"))()
+SetMessage("✅ Hello!", 2)
+SetMessage("⚠️ Alert!", 3, "rbxassetid://6031075938")
+SetMessage("🎁 Bonus!", 4, "rbxassetid://6031074024", Color3.fromRGB(20, 20, 60))
+
+SetRainbowText(true) -- Rainbow only the text
+ SetRainbowBackground(true)
+SetRainbow(true) -- Enable rainbow for both text and background
+--- You Could Use  without  Image or Color
