1bfd5390bde0a7930d6ce2d69a27afeb71cfaa67
javaKL666 <xb6666661979@outlook.com>
2025-12-26 22:00:56 +0800

    Add example for NeKoLolisNotification usage
    Added a notification creation example using NeKoLolisNotification.

---
+++ NeKoLolis/Example.lua
@@ -0,0 +1,9 @@
+local NeKoLolisNotification = loadstring(game:HttpGet(('https://raw.githubusercontent.com/NekoLolis/RobloxNotificationLibrary/main/main.lua')))()
+
+NeKoLolisNotification:CreateNotification("Title", "Text", "type")
+
+--[[
+Title = <string> - The name of the Notification.
+Body = <string> - The Notification Text.
+Type = <string> - The type of notification, success, info or error.
+]]
